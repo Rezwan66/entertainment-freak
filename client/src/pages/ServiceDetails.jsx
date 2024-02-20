dda1a5fe318b374032932232f39856e6dea1ab14
Rezwan66 <shaikhrezwan66@gmail.com>
2024-02-20 18:52:38 +0100

    testing a dynamic banner implementation on the service details page

--- client/src/pages/ServiceDetails.jsx
+++ client/src/pages/ServiceDetails.jsx
@@ -1,5 +1,6 @@
 import toast from 'react-hot-toast';
 import { useLoaderData, useParams } from 'react-router-dom';
+import { DynamicBanner } from '../components/DynamicBanner';
 
 const ServiceDetails = () => {
   const category = useLoaderData();
@@ -13,20 +14,23 @@ const ServiceDetails = () => {
 
   const { image, long_description, name, price } = category || {};
   return (
-    <div className="max-w-7xl mt-20 mb-28 mx-auto px-4 lg:px-0">
-      {/* my card */}
-      <div className=" bg-base-200 bg-opacity-40 rounded-xl gap-4 p-6 md:p-0">
-        <img className="md:w-full h-20 md:rounded-l-xl" src={image} alt="" />
-        <div className="text-white md:p-6 space-y-4">
-          <h1 className="text-4xl font-bold">{name}</h1>
-          <h3 className="text-xl">{long_description}</h3>
-          <br />
-          <button
-            onClick={() => toast.success('Booked Successfully!')}
-            className="btn btn-warning w-full capitalize text-lg"
-          >
-            Book now for only: ${price}
-          </button>
+    <div>
+      <DynamicBanner />
+      <div className="max-w-7xl mt-20 mb-28 mx-auto px-4 lg:px-0">
+        {/* my card */}
+        <div className=" bg-base-200 bg-opacity-40 rounded-xl gap-4 p-6 md:p-0">
+          <img className="md:w-full h-20 md:rounded-l-xl" src={image} alt="" />
+          <div className="text-white md:p-6 space-y-4">
+            <h1 className="text-4xl font-bold">{name}</h1>
+            <h3 className="text-xl">{long_description}</h3>
+            <br />
+            <button
+              onClick={() => toast.success('Booked Successfully!')}
+              className="btn btn-warning w-full capitalize text-lg"
+            >
+              Book now for only: ${price}
+            </button>
+          </div>
         </div>
       </div>
     </div>
