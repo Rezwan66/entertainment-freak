f1a6837dd22a7cd73b33c2826236c3b4caa8d97f
Rezwan66 <shaikhrezwan66@gmail.com>
2024-02-22 18:34:25 +0100

    added todo string

--- client/src/components/DynamicBanner.jsx
+++ client/src/components/DynamicBanner.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react';
 
 export const DynamicBanner = ({ category }) => {
   const [currentSlider, setCurrentSlider] = useState(0);
-  const { image, long_description, name, price } = category || {};
+  const todo = `Create a collection of events and tickets`;
+  const { long_description, name } = category || {};
   const sliders = [
     {
       img: 'https://source.unsplash.com/1200x640/?nature',
