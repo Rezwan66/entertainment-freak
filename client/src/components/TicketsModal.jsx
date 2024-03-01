fc29a7b9a7a64916a956f26e5f53dd5c96f09b2e
Rezwan66 <shaikhrezwan66@gmail.com>
2024-03-01 16:27:40 +0100

    added buy ticket button on the ticket modal

--- client/src/components/TicketsModal.jsx
+++ client/src/components/TicketsModal.jsx
@@ -77,7 +77,7 @@ export default function TicketsModal({ event }) {
                       className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                       onClick={closeModal}
                     >
-                      Got it, thanks!
+                      Buy Ticket Now!
                     </button>
                   </div>
                 </Dialog.Panel>
