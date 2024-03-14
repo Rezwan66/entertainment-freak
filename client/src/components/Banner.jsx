24209ccae6c0bd394f1d56d0e6cbb6e55ecadc4f
Rezwan66 <shaikhrezwan66@gmail.com>
2024-03-14 16:15:51 +0100

    implemented the react responsive carousel on the banner

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,113 +1,172 @@
 import ScrollButton from './ScrollButton';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 const Banner = () => {
   return (
-    <div>
-      <div className="carousel rounded-sm w-full">
-        <div id="slide1" className="carousel-item relative w-full">
-          <img
-            src="/banner.jpg"
-            className="w-full opacity-80 brightness-50 h-[550px] object-cover"
-          />
-          {/* <h2 className="font-bold absolute top-[80%] right-[5%] text-white btn btn-outline">
-            Book today
-          </h2> */}
-          <ScrollButton
-            target="our-services"
-            text="Book Today"
-            classes={
-              'font-bold absolute top-[80%] right-[5%] text-white btn btn-outline'
-            }
-          />
-          <h2 className="text-3xl lg:text-6xl text-white absolute lg:w-[850px] top-[30%] left-[10%]">
-            For the fanciest <span className="text-amber-300">concert</span>{' '}
-            events Implement swiper/react-simple-carousel
-          </h2>
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a
-              href="#slide3"
-              className="btn btn-circle bg-transparent text-white"
-            >
-              ❮
-            </a>
-            <a
-              href="#slide2"
-              className="btn btn-circle bg-transparent text-white"
-            >
-              ❯
-            </a>
-          </div>
-        </div>
-        <div id="slide2" className="carousel-item relative w-full">
-          <img
-            src="/bannerStadium.png"
-            className="w-full opacity-80 brightness-50 h-[550px] object-fill"
-          />
-          {/* <h2 className="font-bold absolute top-[80%] right-[5%] text-white btn btn-outline">
-            Book today
-          </h2> */}
-          <ScrollButton
-            target="our-services"
-            text="Book Today"
-            classes={
-              'font-bold absolute top-[80%] right-[5%] text-white btn btn-outline'
-            }
-          />
-          <h2 className="text-3xl lg:text-6xl text-white absolute top-[30%] left-[10%]">
-            Or the craziest <span className="text-amber-300">sports</span>{' '}
-            events
-          </h2>
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a
-              href="#slide1"
-              className="btn btn-circle bg-transparent text-white"
-            >
-              ❮
-            </a>
-            <a
-              href="#slide3"
-              className="btn btn-circle bg-transparent text-white"
-            >
-              ❯
-            </a>
-          </div>
-        </div>
-        <div id="slide3" className="carousel-item relative w-full">
-          <img
-            src="/bannerTheatre.jpg"
-            className="w-full opacity-80 brightness-50 h-[550px] object-fill"
-          />
-          {/* <h2 className="font-bold absolute top-[80%] right-[5%] text-white btn btn-outline">
-            Book today
-          </h2> */}
-          <ScrollButton
-            target="our-services"
-            text="Book Today"
-            classes={
-              'font-bold absolute top-[80%] right-[5%] text-white btn btn-outline'
-            }
-          />
-          <h2 className="text-3xl lg:text-6xl text-white absolute top-[30%] left-[10%]">
-            Or even silent <span className="text-amber-300">theatre</span>{' '}
-            events
-          </h2>
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a
-              href="#slide2"
-              className="btn btn-circle bg-transparent text-white"
-            >
-              ❮
-            </a>
-            <a
-              href="#slide1"
-              className="btn btn-circle bg-transparent text-white"
-            >
-              ❯
-            </a>
-          </div>
-        </div>
+    // <div>
+    //   <div className="carousel rounded-sm w-full">
+    //     <div id="slide1" className="carousel-item relative w-full">
+    //       <img
+    //         src="/banner.jpg"
+    //         className="w-full opacity-80 brightness-50 h-[550px] object-cover"
+    //       />
+    //       {/* <h2 className="font-bold absolute top-[80%] right-[5%] text-white btn btn-outline">
+    //         Book today
+    //       </h2> */}
+    //       <ScrollButton
+    //         target="our-services"
+    //         text="Book Today"
+    //         classes={
+    //           'font-bold absolute top-[80%] right-[5%] text-white btn btn-outline'
+    //         }
+    //       />
+    //       <h2 className="text-3xl lg:text-6xl text-white absolute lg:w-[850px] top-[30%] left-[10%]">
+    //         For the fanciest <span className="text-amber-300">concert</span>{' '}
+    //         events Implement swiper/react-simple-carousel
+    //       </h2>
+    //       <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+    //         <a
+    //           href="#slide3"
+    //           className="btn btn-circle bg-transparent text-white"
+    //         >
+    //           ❮
+    //         </a>
+    //         <a
+    //           href="#slide2"
+    //           className="btn btn-circle bg-transparent text-white"
+    //         >
+    //           ❯
+    //         </a>
+    //       </div>
+    //     </div>
+    //     <div id="slide2" className="carousel-item relative w-full">
+    //       <img
+    //         src="/bannerStadium.png"
+    //         className="w-full opacity-80 brightness-50 h-[550px] object-fill"
+    //       />
+    //       {/* <h2 className="font-bold absolute top-[80%] right-[5%] text-white btn btn-outline">
+    //         Book today
+    //       </h2> */}
+    //       <ScrollButton
+    //         target="our-services"
+    //         text="Book Today"
+    //         classes={
+    //           'font-bold absolute top-[80%] right-[5%] text-white btn btn-outline'
+    //         }
+    //       />
+    //       <h2 className="text-3xl lg:text-6xl text-white absolute top-[30%] left-[10%]">
+    //         Or the craziest <span className="text-amber-300">sports</span>{' '}
+    //         events
+    //       </h2>
+    //       <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+    //         <a
+    //           href="#slide1"
+    //           className="btn btn-circle bg-transparent text-white"
+    //         >
+    //           ❮
+    //         </a>
+    //         <a
+    //           href="#slide3"
+    //           className="btn btn-circle bg-transparent text-white"
+    //         >
+    //           ❯
+    //         </a>
+    //       </div>
+    //     </div>
+    //     <div id="slide3" className="carousel-item relative w-full">
+    //       <img
+    //         src="/bannerTheatre.jpg"
+    //         className="w-full opacity-80 brightness-50 h-[550px] object-fill"
+    //       />
+    //       {/* <h2 className="font-bold absolute top-[80%] right-[5%] text-white btn btn-outline">
+    //         Book today
+    //       </h2> */}
+    //       <ScrollButton
+    //         target="our-services"
+    //         text="Book Today"
+    //         classes={
+    //           'font-bold absolute top-[80%] right-[5%] text-white btn btn-outline'
+    //         }
+    //       />
+    //       <h2 className="text-3xl lg:text-6xl text-white absolute top-[30%] left-[10%]">
+    //         Or even silent <span className="text-amber-300">theatre</span>{' '}
+    //         events
+    //       </h2>
+    //       <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+    //         <a
+    //           href="#slide2"
+    //           className="btn btn-circle bg-transparent text-white"
+    //         >
+    //           ❮
+    //         </a>
+    //         <a
+    //           href="#slide1"
+    //           className="btn btn-circle bg-transparent text-white"
+    //         >
+    //           ❯
+    //         </a>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+    <Carousel
+      showThumbs={false}
+      infiniteLoop={true}
+      autoPlay={true}
+      interval={2500}
+      showStatus={false}
+    >
+      <div id="slide1">
+        <img
+          src="/banner.jpg"
+          className="w-full opacity-80 brightness-50 h-[550px] object-cover"
+        />
+        <ScrollButton
+          target="our-services"
+          text="Book Today"
+          classes={
+            'font-bold absolute top-[80%] right-[5%] text-white btn btn-outline'
+          }
+        />
+        <h2 className="text-3xl lg:text-6xl text-white absolute lg:w-[850px] top-[30%] left-[10%]">
+          For the fanciest <span className="text-amber-300">concert</span>{' '}
+          events
+        </h2>
       </div>
-    </div>
+      <div id="slide2">
+        <img
+          src="/bannerStadium.png"
+          className="w-full opacity-80 brightness-50 h-[550px] object-fill"
+        />
+        <ScrollButton
+          target="our-services"
+          text="Book Today"
+          classes={
+            'font-bold absolute top-[80%] right-[5%] text-white btn btn-outline'
+          }
+        />
+        <h2 className="text-3xl lg:text-6xl text-white absolute top-[30%] left-[10%]">
+          Or the craziest <span className="text-amber-300">sports</span> events
+        </h2>
+      </div>
+      <div id="slide3">
+        <img
+          src="/bannerTheatre.jpg"
+          className="w-full opacity-80 brightness-50 h-[550px] object-fill"
+        />
+        <ScrollButton
+          target="our-services"
+          text="Book Today"
+          classes={
+            'font-bold absolute top-[80%] right-[5%] text-white btn btn-outline'
+          }
+        />
+        <h2 className="text-3xl lg:text-6xl text-white absolute top-[30%] left-[10%]">
+          Or even silent <span className="text-amber-300">theatre</span> events
+        </h2>
+      </div>
+    </Carousel>
   );
 };
 
