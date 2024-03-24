31ca8ab50338b5485e942858cceb6802784d301e
Rezwan66 <shaikhrezwan66@gmail.com>
2024-03-24 18:08:11 +0100

    implemented React-Marquee to display the artists section

--- client/src/components/Artists.jsx
+++ client/src/components/Artists.jsx
@@ -1,6 +1,7 @@
 // import { useEffect, useState } from 'react';
 // import useAxiosPublic from '../hooks/useAxiosPublic';
 import ArtistCard from './ArtistCard';
+import Marquee from 'react-fast-marquee';
 // import SpinnerSmall from './SpinnerSmall';
 
 const Artists = ({ artists }) => {
@@ -28,11 +29,13 @@ const Artists = ({ artists }) => {
   // console.log(artists);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-between gap-6 px-10 lg:px-0">
-      {artists?.map(artist => (
-        <ArtistCard key={artist._id} artist={artist}></ArtistCard>
-      ))}
-    </div>
+    <Marquee speed={100}>
+      <div className="overflow-y-hidden overflow-x-hidden grid grid-cols-4 justify-between gap-6 px-10 lg:px-0">
+        {artists?.map(artist => (
+          <ArtistCard key={artist._id} artist={artist}></ArtistCard>
+        ))}
+      </div>
+    </Marquee>
   );
 };
 
