92623f49b19038d12d7702e7e35eb5267515c7b3
Rezwan66 <shaikhrezwan66@gmail.com>
2024-03-26 18:05:09 +0100

    edited the animation out of the artist cards

--- client/src/components/ArtistCard.jsx
+++ client/src/components/ArtistCard.jsx
@@ -6,18 +6,18 @@ AOS.init();
 const ArtistCard = ({ artist }) => {
   const { name, image } = artist;
   return (
-    <div data-aos="zoom-out">
-      <div className="card card-side items-center justify-center bg-transparent">
-        <div className="avatar">
-          <div className="w-24 h-24 rounded-full">
-            <img src={image} />
-          </div>
-        </div>
-        <div className="card-body">
-          <h2 className="card-title text-white">{name}</h2>
+    // <div data-aos="zoom-out">
+    <div className="card card-side items-center justify-center bg-transparent">
+      <div className="avatar">
+        <div className="w-24 h-24 rounded-full">
+          <img src={image} />
         </div>
       </div>
+      <div className="card-body">
+        <h2 className="card-title text-white">{name}</h2>
+      </div>
     </div>
+    // </div>
   );
 };
 
