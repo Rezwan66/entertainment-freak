edbe1a1ced1b1e8205c40a38eba6b5190ece3971
Rezwan66 <shaikhrezwan66@gmail.com>
2024-03-27 20:26:58 +0100

    installed react redux and redux toolkit  for ticket quantity count and wrapped the app inside the redux Provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,8 @@ import { RouterProvider } from 'react-router-dom';
 import Router from './routes/Router.jsx';
 import AuthProvider from './providers/AuthProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import store from './redux/store.jsx';
+import { Provider } from 'react-redux';
 
 const queryClient = new QueryClient();
 
@@ -12,7 +14,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <QueryClientProvider client={queryClient}>
-        <RouterProvider router={Router}></RouterProvider>{' '}
+        <Provider store={store}>
+          <RouterProvider router={Router}></RouterProvider>
+        </Provider>
       </QueryClientProvider>
     </AuthProvider>
   </React.StrictMode>
