11e505123d30a4d7980ee904a7c68fcacc3c948d
Rezwan66 <shaikhrezwan66@gmail.com>
2024-04-11 22:47:05 +0200

    created a cartSlice and the store for redux cart provider

--- client/src/redux/store.jsx
+++ client/src/redux/store.jsx
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import ticketReducer from './features/tickets/ticketSlice';
+// import ticketReducer from './features/tickets/ticketSlice';
+import cartSlice from './cartSlice';
 
 const store = configureStore({
   reducer: {
-    tickets: ticketReducer,
+    cart: cartSlice,
   },
 });
 
